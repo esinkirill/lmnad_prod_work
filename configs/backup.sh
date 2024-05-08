d12403eb556a68ec10452aa3e431dbc13d156d33
Kirill <kirill.esin.2017@gmail.com>
2024-05-08 20:33:43 +0300

    work backup

--- configs/backup.sh
+++ configs/backup.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-mysqldump=/usr/bin/mysqldump
 mysql_user=root
 mysql_password=78910
 
-basedir=/root/backup
-backup_dir=files
-
-# Clear current backups
-cd $basedir/$backup_dir
-rm -f backup*
-
 now=$(date +"%d_%m_%Y")
 
-# Make mysql backup
-$mysqldump --user=$mysql_user --password=$mysql_password --triggers --routines --databases lmnad_db | gzip > backup_$now.sql.gz
+docker exec lmnad_web sh -c 'mkdir -p /root/backup && rm -f /root/backup/* && tar cvzf /root/backup/backup.lmnad_uploads_$1.tar.gz /lmnad/project/media/' sh "$now"
 
-# Make files backup
-tar cvzf backup.lmnad_uploads_$now.tar.gz /lmnad/project/media/
+docker exec lmnad_mysql sh -c 'mkdir -p /root/backup && rm -f /root/backup/* && mysqldump --user=$1 --password=$2 --triggers --routines --databases lmnad_db | gzip > /root/backup/backup_$3.sql.gz' sh "$mysql_user" "$mysql_password" "$now"
 
+docker cp lmnad_mysql:/root/backup/backup_$now.sql.gz /tmp/
+
+docker cp /tmp/backup_$now.sql.gz lmnad_web:/root/backupcd
