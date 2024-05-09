f32755d19a1d1d07ac8fdd2222967b3370053251
esinkirill <78916798+esinkirill@users.noreply.github.com>
2024-05-09 18:47:30 +0300

    Update backup.sh

--- configs/backup.sh
+++ configs/backup.sh
@@ -2,13 +2,18 @@
 
 mysql_user=root
 mysql_password=78910
-
 now=$(date +"%d_%m_%Y")
 
-docker exec lmnad_web sh -c 'mkdir -p /root/backup && rm -f /root/backup/* && tar cvzf /root/backup/backup.lmnad_uploads_$1.tar.gz /lmnad/project/media/' sh "$now"
-
-docker exec lmnad_mysql sh -c 'mkdir -p /root/backup && rm -f /root/backup/* && mysqldump --user=$1 --password=$2 --triggers --routines --databases lmnad_db | gzip > /root/backup/backup_$3.sql.gz' sh "$mysql_user" "$mysql_password" "$now"
+docker exec lmnad_mysql sh -c '
+    mkdir -p /root/backup &&
+    rm -f /root/backup/* &&
+    mysqldump --user=$1 --password=$2 --triggers --routines --databases lmnad_db | gzip > /root/backup/backup_$3.sql.gz' sh "$mysql_user" "$mysql_password" "$now"
 
 docker cp lmnad_mysql:/root/backup/backup_$now.sql.gz /tmp/
 
 docker cp /tmp/backup_$now.sql.gz lmnad_web:/root/backup
+
+docker exec lmnad_web sh -c '
+    mkdir -p /root/backup &&
+    tar cvzf /root/backup/backup.lmnad_uploads_$1.tar.gz /lmnad/project/media/ &&
+    find /root/backup/* -mtime +7 -exec rm {} \;' sh "$now"
